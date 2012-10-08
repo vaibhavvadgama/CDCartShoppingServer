a3fae4080482abd4bebb3847d494d5decf1534e1
Utkarsh <Utkarsh@Utkarsh-PC>
2012-10-07 21:30:07 -0300

    added getProduct and getProductInfo functions

--- src/java/com/cdstoreserver/ws/productcatalog/ProductCatalogWS.java
+++ src/java/com/cdstoreserver/ws/productcatalog/ProductCatalogWS.java
@@ -8,10 +8,9 @@ import com.cdstoreserver.dbagent.beans.CDList;
 import com.cdstoreserver.dbagent.beans.CategoryList;
 import com.cdstoreserver.dbagent.dao.CategoryDao;
 import com.cdstoreserver.dbagent.dao.CdDao;
-import java.util.ArrayList;
-import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebService;
 
 /**
  *
@@ -38,15 +37,44 @@ public class ProductCatalogWS {
         CDList responseObj = new CDList();
         
         CdDao dao = new CdDao();
-        ArrayList items = dao.getProductList(categoryId);
         
-//        for(CdBean item : items) {
-//            responseObj.cdList.add(item);
-//        }
-//        responseObj.cdList.add(new CDItem(1, "Linkin Park", 10.99, 2, "cd1.png", "25/10/2012 11:00:35", true));
-//        responseObj.cdList.add(new CDItem(2, "Album 2", 12.99, 2, "cd2.png", "26/10/2012 12:00:35", true));
+        responseObj.cd =  dao.getProductList(categoryId);
         
-        return responseObj;
+        
+        if(responseObj.cd.size() == 0) {
+            responseObj.status = "error";
+            responseObj.errormessage = "No product found!";
+        } else {
+            responseObj.status = "success";
+            responseObj.errormessage = "";
+        }
+        
+        return responseObj;       
+        
+    }
+    
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getProductInfo")
+    public CDList getProductInfo(@WebParam(name = "productId") Integer productId) {
+        //TODO write your implementation code here:
+        CDList responseObj = new CDList();
+        
+        CdDao dao = new CdDao();
+        
+        responseObj.cd =  dao.getProductInfo(productId);
+        
+        if(responseObj.cd.size() == 0) {
+            responseObj.status = "error";
+            responseObj.errormessage = "No product data found!";
+        } else {
+            responseObj.status = "success";
+            responseObj.errormessage = "";
+        }
+        
+        return responseObj;       
+        
     }
     
     @WebMethod(operationName = "getCategoryList")
